d8ec2afe3e1517f43f78e59eca92ddb571f5339e
beerpiss <lacvtg.a1.2023@gmail.com>
2022-03-27 19:07:36 +0700

    various improvements
    * feat: adds a timeout for the SSH connection
    * feat: handle KeyboardInterrupt
    * feat: use a temporary directory for saving the raw img4 blob
    * refactor: merge some multiline prints

--- deverser.py
+++ deverser.py
@@ -1,30 +1,34 @@
 import pyimg4
 import plistlib
-import getpass
-import os
-import sys
 import paramiko.ssh_exception
 import pyasn1.codec.der.decoder
+import socket
+import tempfile
+import os.path
 from rich import print
+from rich.prompt import Prompt
+from os import remove
+from sys import exit
 from fabric import Connection
 from typing import Tuple
 
 
 def interactive_input() -> Tuple[str, str, int]:
-    print("Welcome to Deverser")
     print(
-        "This script will dump blobs from your [bold]jailbroken[/bold] iOS device and copy it to your computer."
+        """Welcome to Deverser
+
+This script will dump blobs from your [bold]jailbroken[/bold] iOS device and copy it to your computer.
+Depending on the blob, you might [bold]not[/bold] be able to use it without a bootROM exploit (e.g. checkm8).
+Refer to https://ios.cfw.guide/saving-blobs/#saving-onboard-blobs for more information.""",
+        end="\n\n",
     )
     print(
-        "Depending on the blob, you might not be able to use it without a bootROM exploit (e.g. checkm8)."
+        "Options given out in [bold blue](blue parentheses)[/bold blue] are default values and will be used if you haven't specified an option.",
+        end="\n\n",
     )
-    print(
-        "Refer to https://ios.cfw.guide/saving-blobs/#saving-onboard-blobs for more information."
-    )
-    print("")
 
     while True:
-        device_addr = input("Enter the IP address of your device: ")
+        device_addr = Prompt.ask("[bold]Enter the device's IP address[/bold]")
         if device_addr == "":
             print("Please enter an IP address.")
             continue
@@ -33,47 +37,49 @@ def interactive_input() -> Tuple[str, str, int]:
     print("")
 
     print("Please note that it is normal for the password to [bold]not[/bold] appear.")
-    password = (
-        getpass.getpass("Enter the root user password (default is 'alpine'): ")
-        or "alpine"
+    password = Prompt.ask(
+        "[bold]Enter the root user password[/bold]", password=True, default="alpine"
     )
     print("")
 
-    sshport = int(input("Enter the SSH port of your device (default is 22): ") or 22)
+    sshport = int(Prompt.ask("[bold]Enter the SSH port[/bold]", default="22"))
     return (device_addr, password, sshport)
 
 
 def main():
     device_addr, password, sshport = interactive_input()
 
-    try:
-        with Connection(
-            device_addr,
-            user="root",
-            port=sshport,
-            connect_kwargs={"password": password},
-        ) as c:
-            c.run("dd if=/dev/disk1 of=dump.raw bs=256 count=$((0x4000))")
-            c.get("dump.raw")
-    except paramiko.ssh_exception.NoValidConnectionsError:
-        print("[red]Could not connect to device![/red]")
-        return 1
-    except paramiko.ssh_exception.AuthenticationException:
-        print("[red]Could not authenticate with your device![/red]")
-        print("[red]Please check if:[/red]")
-        print("[red] - You entered the correct password[/red]")
-        print("[red] - You have not disabled root login[/red]")
-        return 1
-    except paramiko.ssh_exception.SSHException:
-        print("[red]An SSH2 error occured.[/red]")
-        return 1
+    with tempfile.TemporaryDirectory() as tmpdir:
+        try:
+            with Connection(
+                device_addr,
+                user="root",
+                port=sshport,
+                connect_kwargs={
+                    "password": password,
+                    "timeout": 10.0,
+                },
+            ) as c:
+                c.run("dd if=/dev/disk1 of=dump.raw bs=256 count=$((0x4000))")
+                c.get("dump.raw", os.path.join(tmpdir, "dump.raw"))
+        except paramiko.ssh_exception.NoValidConnectionsError:
+            print("[red]Could not connect to device.[/red]")
+            return 1
+        except paramiko.ssh_exception.AuthenticationException:
+            print("[red]Could not authenticate with your device.[/red]")
+            print("[red]Please check if:[/red]")
+            print("[red] - You entered the correct password[/red]")
+            print("[red] - You have not disabled root login[/red]")
+            return 1
+        except paramiko.ssh_exception.SSHException:
+            print("[red]An SSH2 error occured.[/red]")
+            return 1
+        except socket.timeout:
+            print("[red]Timed out trying to connect.[/red]")
+            return 1
 
-    with open("dump.raw", "rb") as f:
-        img4, _ = pyasn1.codec.der.decoder.decode(f.read())
-    try:
-        os.remove("dump.raw")
-    except FileNotFoundError:
-        pass
+        with open("dump.raw", "rb") as f:
+            img4, _ = pyasn1.codec.der.decoder.decode(f.read())
 
     im4m = pyimg4.get_im4m_from_img4(img4)
     ecid = pyimg4.get_value_from_im4m(im4m, "ECID")
@@ -96,4 +102,8 @@ def main():
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    try:
+        exit(main())
+    except KeyboardInterrupt:
+        print("\n[red]Aborted by user.[/red]")
+        exit(1)
